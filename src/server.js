2249d99bfb21bc5475b5d52cca8bcb4766f0b545
Alex <31661440+am2367@users.noreply.github.com>
2021-05-15 23:44:52 -0400

    Fixing hosting of client in heroku

--- src/server.js
+++ src/server.js
@@ -34,6 +34,7 @@ app.use(session({
     prefix: `session:`,
   })
 }));
+app.use(express.static(`${__dirname}/client/build`));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
